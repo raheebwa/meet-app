b792e04f461bc786fde6082cfd8c4dfc8ac8d27d
Aheebwa Ramadhan <raaheebwa@gmail.com>
2023-02-28 12:13:22 +0300

    feature: add details

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Made Some Changes
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello World
+          Hello World, Ramadhan
         </a>
       </header>
     </div>
